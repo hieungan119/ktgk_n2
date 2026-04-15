63f36d97664232dbaeea468f68f28fea5062821f
Huỳnh Thị Quỳnh Như <huynhthiquynhnhu56@gmail.com>
2026-04-15 15:07:15 +0700

    Quỳnh Như

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
-
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-
 Route::get('/dashboard', function () {
-    //return view('dashboard');
+
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
 require __DIR__.'/auth.php';
+
+Route::get('/caycanh/list', [ManagementController::class, 'list_caycanh'])->name('caycanh.list');
+
+Route::post('/caycanh/delete/{id}', [ManagementController::class, 'delete_caycanh'])->name('caycanh.delete');
+
+Route::get('/caycanh/detail/{id}', [ManagementController::class, 'detail_caycanh'])->name('caycanh.detail');
